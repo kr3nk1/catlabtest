c63dad26f06e54e76722829609da361341a5fef6
kr3nk1 <jv.garrido99@gmail.com>
2023-05-02 23:53:43 -0400

    added cat likes function

---
+++ cats.js
@@ -0,0 +1,10 @@
+//this program will tell us about out cats
+let cats =[
+{name: 'taby', likes: 'catni'},
+{name: 'bob ross', likes: 'paint'},
+{name: 'chaos', likes: 'knocking over plants'}
+]
+
+for(let i=0; i<cats.length; i++) {
+  console.log(`${cats[i].name} likes ${cats[i].likes}`);
+}
